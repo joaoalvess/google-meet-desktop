72d3d532220c60f2a5dc7b2f43da9d817b790cf4
Joao Alves <jp14700@gmail.com>
2021-05-10 20:11:33 -0300

    add react-router-dom

---
+++ src/routes/index.routes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Dashboard from '../pages/dashboard'
+
+export default function App() {
+  return (
+    <Router>
+        <Switch>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+    </Router>
+  );
+}
